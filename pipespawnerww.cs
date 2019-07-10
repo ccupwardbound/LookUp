04b0b7ff7fc07529114797d093ab3f2c6f64540b
feraxen <NeffinatorCheddah@gmail.com>
2019-07-10 13:53:52 -0700

    pipe spawner for flappy bird
    pipe spwaner cause y'all lazy.

---
+++ pipespawnerww.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public float maxTime = 1;
+    public float timer = 0;
+    public GameObject PIPE;
+    public float height;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newPIPE = Instantiate(PIPE);
+        newPIPE.transform.position = transform.position + new Vector3(0, Random.Range(-1, 3), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > maxTime)
+        {
+            GameObject newPIPE = Instantiate(PIPE);
+            newPIPE.transform.position = transform.position + new Vector3(0, Random.Range(-1, 3), 0);
+            Destroy(newPIPE, 10);
+            timer = 0;
+        }
+        timer += Time.deltaTime; 
+    }
+}
